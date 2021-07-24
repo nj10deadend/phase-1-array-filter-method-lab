70735ef139166c2842bda459b10e1d7100e21111
nj10deadend <nasjacks10@gmail.com>
2021-07-24 18:03:26 -0400

    commit tree 2fd383f05b205e46b9f8cccbbb446fd9d30ea772

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+// function findMatching(drivers, driverName) {
+//     drivers.filter(function() {return driverName === 'Bobby'})
+// }
+// const findMatching = drivers.filter(driverName => driverName==='Bobby')
+
+function findMatching(drivers, name) {
+    return drivers.filter(name => name ==='Bobby')
+}
